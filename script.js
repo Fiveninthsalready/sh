63b8bf8e8181f76be2f79f6e2fee234a7bd74e36
Fiveninthsalready <143618733+Fiveninthsalready@users.noreply.github.com>
2024-02-29 15:39:00 +0000

    Quiz

---
+++ script.js
@@ -0,0 +1,11 @@
+let buttonOne = document.querySelector(".buttonOne");
+let buttonTwo = document.querySelector(".buttonTwo");
+let Question1 = document.querySelector(".Question1");
+
+
+buttonOne.addEventListener ("click", function() {
+buttonOne.style.display = "none";
+buttonTwo.style.display = "none";
+Question1.style.display = "none";
+});
+
