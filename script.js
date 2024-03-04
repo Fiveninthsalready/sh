b5c89395bb4e24057fa26899ca570895019ec229
Fiveninthsalready <143618733+Fiveninthsalready@users.noreply.github.com>
2024-03-04 15:41:25 +0000

    c

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let buttonOne = document.querySelector(".buttonOne");
-let buttonTwo = document.querySelector(".buttonTwo");
 let Question1 = document.querySelector(".Question1");
+let messade = document.querySelector(".message");
 
 
 buttonOne.addEventListener ("click", function() {
-buttonOne.style.display = "none";
-buttonTwo.style.display = "none";
-Question1.style.display = "none";
+ let answer1 = document.querySelector(".answer1").value;
 });
+
+if(answer1 === "Born Rich"){
+ message.innerHTML = "greedy";
+}
 
